fe958476868f4c7b7152b16e910afafeb760cd43
Raj Gupta <rgupta7941@gmail.com>
2026-06-25 20:34:36 +0530

    code2

---
+++ day 25/code25,2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100];
+    int i, j, found;
+
+    printf("Enter first string: ");
+    gets(str1);
+
+    printf("Enter second string: ");
+    gets(str2);
+
+    printf("Common characters are: ");
+
+    for(i = 0; str1[i] != '\0'; i++) {
+        found = 0;
+
+        for(j = 0; j < i; j++) {
+            if(str1[i] == str1[j]) {
+                found = 1;
+                break;
+            }
+        }
+
+        if(found)
+            continue;
+
+        for(j = 0; str2[j] != '\0'; j++) {
+            if(str1[i] == str2[j]) {
+                printf("%c ", str1[i]);
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
