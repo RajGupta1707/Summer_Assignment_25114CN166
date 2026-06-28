0c1d00e97a41b7eb8a33c171130773a5811c9d38
Raj Gupta <rgupta7941@gmail.com>
2026-06-28 13:28:13 +0530

    code2

---
+++ day 28/code28,2.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main() {
+    int accNo, choice;
+    char name[50];
+    float balance = 0, amount;
+
+    printf("Enter Account Number: ");
+    scanf("%d", &accNo);
+
+    printf("Enter Account Holder Name: ");
+    scanf("%s", name);
+
+    do {
+        printf("\n----- Bank Menu -----\n");
+        printf("1. Deposit\n");
+        printf("2. Withdraw\n");
+        printf("3. Check Balance\n");
+        printf("4. Exit\n");
+        printf("Enter Choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1:
+                printf("Enter Deposit Amount: ");
+                scanf("%f", &amount);
+                balance += amount;
+                break;
+
+            case 2:
+                printf("Enter Withdrawal Amount: ");
+                scanf("%f", &amount);
+                if(amount <= balance)
+                    balance -= amount;
+                else
+                    printf("Insufficient Balance!\n");
+                break;
+
+            case 3:
+                printf("Current Balance = %.2f\n", balance);
+                break;
+
+            case 4:
+                printf("Thank You!\n");
+                break;
+
+            default:
+                printf("Invalid Choice!\n");
+        }
+
+    } while(choice != 4);
+
+    return 0;
+}
